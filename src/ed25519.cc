6356bc9e6e4643d56020293c14920ac12bcaa1ac
Kenan Sulayman <kenan@sly.mn>
2015-09-15 00:39:23 +0200

    Update nan, migrate to nan2

--- src/ed25519.cc
+++ src/ed25519.cc
@@ -15,26 +15,26 @@ using namespace node;
  * returns: an Object with PublicKey and PrivateKey
  **/
 NAN_METHOD(MakeKeypair) {
-	NanScope();
-	if ((args.Length() < 1) || (!Buffer::HasInstance(args[0])) || (Buffer::Length(args[0]->ToObject()) != 32)) {
-		return NanThrowError("MakeKeypair requires a 32 byte buffer");
+	Nan::HandleScope scope;
+	if ((info.Length() < 1) || (!Buffer::HasInstance(info[0])) || (Buffer::Length(info[0]->ToObject()) != 32)) {
+		return Nan::ThrowError("MakeKeypair requires a 32 byte buffer");
 	}
-	const unsigned char* seed = (unsigned char*)Buffer::Data(args[0]->ToObject());
+	const unsigned char* seed = (unsigned char*)Buffer::Data(info[0]->ToObject());
 
-	v8::Local<v8::Object> privateKey = NanNewBufferHandle(64);
+	v8::Local<v8::Object> privateKey = Nan::NewBuffer(64).ToLocalChecked();
 
 	unsigned char* privateKeyData = (unsigned char*)Buffer::Data(privateKey);
 
-	v8::Local<v8::Object> publicKey = NanNewBufferHandle(32);
+	v8::Local<v8::Object> publicKey = Nan::NewBuffer(32).ToLocalChecked();
 	unsigned char* publicKeyData = (unsigned char*)Buffer::Data(publicKey);
 	for (int i = 0; i < 32; i++)
 		privateKeyData[i] = seed[i];
 	crypto_sign_keypair(publicKeyData, privateKeyData);
 
-	Local<Object> result = NanNew<Object>();
-	result->Set(NanNew("publicKey"), publicKey);
-	result->Set(NanNew("privateKey"), privateKey);
-	NanReturnValue(result);
+	Local<Object> result = Nan::New<Object>();
+	result->Set(Nan::New("publicKey").ToLocalChecked(), publicKey);
+	result->Set(Nan::New("privateKey").ToLocalChecked(), privateKey);
+	info.GetReturnValue().Set(result);
 }
 
 /**
@@ -47,13 +47,13 @@ NAN_METHOD(MakeKeypair) {
  * returns: the signature as a Buffer
  **/
 NAN_METHOD(Sign) {
-	NanScope();
-	if ((args.Length() < 2) || (!Buffer::HasInstance(args[0]->ToObject()))) {
-		return NanThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
+	Nan::HandleScope scope;
+	if ((info.Length() < 2) || (!Buffer::HasInstance(info[0]->ToObject()))) {
+		return Nan::ThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
 	}
 	unsigned char* privateKey;
-	if ((Buffer::HasInstance(args[1])) && (Buffer::Length(args[1]->ToObject()) == 32)) {
-		unsigned char* seed = (unsigned char*)Buffer::Data(args[1]->ToObject());
+	if ((Buffer::HasInstance(info[1])) && (Buffer::Length(info[1]->ToObject()) == 32)) {
+		unsigned char* seed = (unsigned char*)Buffer::Data(info[1]->ToObject());
 		unsigned char publicKeyData[32];
 		unsigned char privateKeyData[64];
 		for (int i = 0; i < 32; i++) {
@@ -61,31 +61,31 @@ NAN_METHOD(Sign) {
 		}
 		crypto_sign_keypair(publicKeyData, privateKeyData);
 		privateKey = privateKeyData;
-	} else if ((Buffer::HasInstance(args[1])) && (Buffer::Length(args[1]->ToObject()) == 64)) {
-		privateKey = (unsigned char*)Buffer::Data(args[1]->ToObject());
-	} else if ((args[1]->IsObject()) && (!Buffer::HasInstance(args[1]))) {
-		Handle<Object> privateKeyBuffer = args[1]->ToObject()->Get(NanNew<String>("privateKey"))->ToObject();
+	} else if ((Buffer::HasInstance(info[1])) && (Buffer::Length(info[1]->ToObject()) == 64)) {
+		privateKey = (unsigned char*)Buffer::Data(info[1]->ToObject());
+	} else if ((info[1]->IsObject()) && (!Buffer::HasInstance(info[1]))) {
+		Handle<Object> privateKeyBuffer = info[1]->ToObject()->Get(Nan::New<String>("privateKey").ToLocalChecked())->ToObject();
 		if (!Buffer::HasInstance(privateKeyBuffer)) {
-			return NanThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
+			return Nan::ThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
 		}
 		privateKey = (unsigned char*)Buffer::Data(privateKeyBuffer);
 	} else {
-		return NanThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
+		return Nan::ThrowError("Sign requires (Buffer, {Buffer(32 or 64) | keyPair object})");
 	}
-	Handle<Object> message = args[0]->ToObject();
+	Handle<Object> message = info[0]->ToObject();
 	const unsigned char* messageData = (unsigned char*)Buffer::Data(message);
 	size_t messageLen = Buffer::Length(message);
 	unsigned long long sigLen = 64 + messageLen;
 	unsigned char *signatureMessageData = (unsigned char*) malloc(sigLen);
 	crypto_sign(signatureMessageData, &sigLen, messageData, messageLen, privateKey);
 
-	v8::Local<v8::Object> signature = NanNewBufferHandle(64);
+	v8::Local<v8::Object> signature = Nan::NewBuffer(64).ToLocalChecked();
 	unsigned char* signatureData = (unsigned char*)Buffer::Data(signature);
 	for (int i = 0; i < 64; i++) {
 		signatureData[i] = signatureMessageData[i];
 	}
 	free(signatureMessageData);
-	NanReturnValue(signature);
+	info.GetReturnValue().Set(signature);
 }
 
 /**
@@ -96,30 +96,30 @@ NAN_METHOD(Sign) {
  * returns: boolean
  **/
 NAN_METHOD(Verify) {
-	NanScope();
-	if ((args.Length() < 3) || (!Buffer::HasInstance(args[0]->ToObject())) ||
-		(!Buffer::HasInstance(args[1]->ToObject())) || (!Buffer::HasInstance(args[2]->ToObject()))) {
-		return NanThrowError("Verify requires (Buffer, Buffer(64), Buffer(32)");
+	Nan::HandleScope scope;
+	if ((info.Length() < 3) || (!Buffer::HasInstance(info[0]->ToObject())) ||
+		(!Buffer::HasInstance(info[1]->ToObject())) || (!Buffer::HasInstance(info[2]->ToObject()))) {
+		return Nan::ThrowError("Verify requires (Buffer, Buffer(64), Buffer(32)");
 	}
-	Handle<Object> message = args[0]->ToObject();
-	Handle<Object> signature = args[1]->ToObject();
-	Handle<Object> publicKey = args[2]->ToObject();
+	Handle<Object> message = info[0]->ToObject();
+	Handle<Object> signature = info[1]->ToObject();
+	Handle<Object> publicKey = info[2]->ToObject();
 	if ((Buffer::Length(signature) != 64) || (Buffer::Length(publicKey) != 32)) {
-		return NanThrowError("Verify requires (Buffer, Buffer(64), Buffer(32)");
+		return Nan::ThrowError("Verify requires (Buffer, Buffer(64), Buffer(32)");
 	}
 	unsigned char* messageData = (unsigned char*)Buffer::Data(message);
 	size_t messageLen = Buffer::Length(message);
 	unsigned char* signatureData = (unsigned char*)Buffer::Data(signature);
 	unsigned char* publicKeyData = (unsigned char*)Buffer::Data(publicKey);
 
-	NanReturnValue(NanNew(crypto_sign_verify(signatureData, messageData, messageLen, publicKeyData) == 0));
+	info.GetReturnValue().Set(Nan::New(crypto_sign_verify(signatureData, messageData, messageLen, publicKeyData) == 0));
 }
 
 
 void InitModule(Handle<Object> exports) {
-	exports->Set(NanNew("MakeKeypair"), NanNew<FunctionTemplate>(MakeKeypair)->GetFunction());
-	exports->Set(NanNew("Sign"), NanNew<FunctionTemplate>(Sign)->GetFunction());
-	exports->Set(NanNew("Verify"), NanNew<FunctionTemplate>(Verify)->GetFunction());
+	exports->Set(Nan::New("MakeKeypair").ToLocalChecked(), Nan::New<FunctionTemplate>(MakeKeypair)->GetFunction());
+	exports->Set(Nan::New("Sign").ToLocalChecked(), Nan::New<FunctionTemplate>(Sign)->GetFunction());
+	exports->Set(Nan::New("Verify").ToLocalChecked(), Nan::New<FunctionTemplate>(Verify)->GetFunction());
 }
 
 NODE_MODULE(ed25519, InitModule)
